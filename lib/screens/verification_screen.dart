329d3553b27117ef2a6ea19d48bff8dfbb0c57de
mak55533 <kuznietsov.makar@gmail.com>
2020-08-21 14:39:03 +0300

    added verification screen

--- lib/screens/verification_screen.dart
+++ lib/screens/verification_screen.dart
@@ -1,64 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:Dumka/utils/const.dart';
+
 class VerificationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: UIConfig.deep_purple_300,
-
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(80),
-              child: const Text(
+            const Padding(
+              padding: EdgeInsets.all(80),
+              child: Text(
                 'Dumka ?',
                 style: TextStyle(color: Colors.white, fontSize: 32),
               ),
             ),
             Container(
-              padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
-              height:601.11,
+              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+              height: 601.11,
               width: double.infinity,
-              decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
+              decoration: const BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(25.0)),
                   color: Colors.white),
               child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  mainAxisSize: MainAxisSize.max,
                   children: <Widget>[
-
-                    Text('Верифікація', style: TextStyle(fontSize: 24),),
-                    Image(
+                    const Text(
+                      'Верифікація',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    const Image(
                       image: AssetImage('assets/verification_photo.png'),
                     ),
-                    TextField(
+                    const TextField(
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: 'Код доступу',
                       ),
                       keyboardType: TextInputType.number,
                     ),
-                    Text(' Для того щоб активувати аккаунт.... Для того щоб активувати аккаунт.... Для того щоб активувати аккаунт.... Для того щоб активувати аккаунт....'),
+                    const Text(
+                        ' Для того щоб активувати аккаунт.... Для того щоб активувати аккаунт.... Для того щоб активувати аккаунт.... Для того щоб активувати аккаунт....'),
                     Container(
-                      width: 143.06 ,
+                      width: 143.06,
                       child: FlatButton(
-                        onPressed: (){},
+                        onPressed: () {},
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(18.0)),
-                        child: Text(
+                        disabledColor: UIConfig.deep_purple_300,
+                        child: const Text(
                           'Продовжити',
                           style: TextStyle(color: Colors.white),
                         ),
-                        disabledColor: UIConfig.deep_purple_300,
                       ),
                     )
-
                   ]),
             )
           ],
