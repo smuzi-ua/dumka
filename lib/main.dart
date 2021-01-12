a09644412704a0ca5f6bcf41d96e127fd1abf7d8
Roman Sytnyk <r.sytnyk@outlook.com>
2021-01-12 23:26:23 +0200

    Firebase (Android) - Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,39 @@
 import 'package:dumka/utils/const.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'screens/splash_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        // if (snapshot.hasError) {
+        //   return SomethingWentWrong();
+        // }
+
+        // Once complete, show your application
+        // if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        // }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        // return Loading();
+      },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
